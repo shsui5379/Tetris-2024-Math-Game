ae896151543edd2beb4227139223f7dd12427bab
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-01-07 23:53:59 -0500

    Revert "constructor and display methods"
    This reverts commit 14b2f45026b2c846365938cf1ba8ae3fb9f07c0b.

--- Grid.ts
+++ Grid.ts
@@ -1,45 +1,13 @@
-class Grid {
+class Grid{
     #numRow: number;
     #numCol: number;
-    #grid: Tile[][];
 
-    /**
-     * Constructs a Grid with the given dimensions, and render it on the display
-     * @constructor
-     * 
-     * @param {number} row  The number of rows for this Grid
-     * @param {number} column  The number of columns for this Grid
-     * @param {HTMLDivElement} element  The HTML div element to render this Grid on
-     */
-    constructor(row: number, column: number, element: HTMLDivElement) {
+    constructor(row: number, column: number, element: HTMLDivElement){
         this.#numRow = row;
         this.#numCol = column;
-        this.#grid = [];
-
-        for (let r = 0; r < this.#numRow; r++) {
-            let rowOfTiles: Tile[] = [];
-
-            for (let c = 0; c < this.#numCol; c++) {
-                rowOfTiles.push(new Tile(element));
-            }
-
-            this.#grid.push(rowOfTiles);
-
-            element.appendChild(document.createElement("br"));
-        }
     }
 
-    /**
-     * Updates the display to reflect the current state of its Tiles
-     */
-    display(): void {
-        for (let row of this.#grid) {
-            for (let tile of row) {
-                tile.display();
-            }
-        }
-    }
-
+    // display(){}
     // moveTiles(){}
     // dropRandomNumber(){}
     // clear(){}
