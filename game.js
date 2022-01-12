815824414267b34ef451d2aad38e7c3120522249
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-01-11 23:45:18 -0500

    message and score display

--- game.js
+++ game.js
@@ -19,11 +19,18 @@ function initializeGame() {
     if (!localStorage.getItem("highscore")) {
         localStorage.setItem("highscore", score.toString());
     }
+    displayScore(score, parseInt(localStorage.getItem("highscore")));
 }
 function startGame() {
     score = 0;
     ongoing = true;
-    grid.dropRandomNumber();
+    if (!grid.dropRandomNumber()) {
+        ongoing = false;
+        printOnMessageBoard("Game over");
+        if (score > parseInt(localStorage.getItem("highscore"))) {
+            localStorage.setItem("highscore", score.toString());
+        }
+    }
 }
 /**
  * Ends the current game, and starts a new one
@@ -31,6 +38,14 @@ function startGame() {
 function reset() {
     if (confirm("Are you sure you want to start a new game?")) {
         console.log("user consented to restart");
+        grid.clear();
+        ongoing = true;
+        //score stuff
+        if (score > parseInt(localStorage.getItem("highscore"))) {
+            localStorage.setItem("highscore", score.toString());
+        }
+        score = 0;
+        displayScore(score, parseInt(localStorage.getItem("highscore")));
     }
 }
 /**
@@ -38,14 +53,16 @@ function reset() {
  * @param {KeyboardEvent} e Details on the keypress
  */
 function keyHandler(e) {
-    if (e.key == "ArrowDown") {
-        console.log("down key pressed");
-    }
-    else if (e.key == "ArrowLeft") {
-        console.log("left key pressed");
-    }
-    else if (e.key == "ArrowRight") {
-        console.log("right key pressed");
+    if (ongoing) {
+        if (e.key == "ArrowDown") {
+            console.log("down key pressed");
+        }
+        else if (e.key == "ArrowLeft") {
+            console.log("left key pressed");
+        }
+        else if (e.key == "ArrowRight") {
+            console.log("right key pressed");
+        }
     }
 }
 /**
@@ -53,26 +70,50 @@ function keyHandler(e) {
  * @param {TouchEvent} e Details on the touch
  */
 function swipeHandler(e) {
-    if (e.type == "touchstart") {
-        touchX = e.touches[0].clientX;
-        touchY = e.touches[0].clientY;
-    }
-    else if (e.type == "touchend") {
-        let xDiff = e.changedTouches[0].clientX - touchX;
-        let yDiff = e.changedTouches[0].clientY - touchY;
-        if (Math.abs(xDiff) > Math.abs(yDiff)) { //horizontal swipe
-            if (xDiff > 0) {
-                console.log("swiped right");
+    if (ongoing) {
+        if (e.type == "touchstart") {
+            touchX = e.touches[0].clientX;
+            touchY = e.touches[0].clientY;
+        }
+        else if (e.type == "touchend") {
+            let xDiff = e.changedTouches[0].clientX - touchX;
+            let yDiff = e.changedTouches[0].clientY - touchY;
+            if (Math.abs(xDiff) > Math.abs(yDiff)) { //horizontal swipe
+                if (xDiff > 0) {
+                    console.log("swiped right");
+                }
+                else {
+                    console.log("swiped left");
+                }
             }
             else {
-                console.log("swiped left");
-            }
-        }
-        else {
-            if (yDiff > 0) {
-                console.log("swiped down");
+                if (yDiff > 0) {
+                    console.log("swiped down");
+                }
             }
         }
     }
+}
+/**
+ * Changes the current merge condition
+ */
+function changeCondition() {
+    printOnMessageBoard(currentCondition.toString());
+}
+/**
+ * Prints a message on the message board
+ * @param {string} message The message to print
+ */
+function printOnMessageBoard(message) {
+    document.getElementById("condition").innerText = message;
+}
+/**
+ * Displays scores on the scoreboard
+ * @param {number} current The current score
+ * @param {number} highscore The high score
+ */
+function displayScore(current, highscore) {
+    document.getElementById("score").innerText = current.toString();
+    document.getElementById("highscore").innerText = highscore.toString();
 }
 //# sourceMappingURL=game.js.map
--- game.ts
+++ game.ts
@@ -22,12 +22,19 @@ function initializeGame(): void {
     if (!localStorage.getItem("highscore")) {
         localStorage.setItem("highscore", score.toString());
     }
+    displayScore(score, parseInt(<string>localStorage.getItem("highscore")));
 }
 
 function startGame(): void {
     score = 0;
     ongoing = true;
-    grid.dropRandomNumber();
+    if (!grid.dropRandomNumber()) {
+        ongoing = false;
+        printOnMessageBoard("Game over");
+        if (score > parseInt(<string>localStorage.getItem("highscore"))) {
+            localStorage.setItem("highscore", score.toString());
+        }
+    }
 }
 
 /**
@@ -36,6 +43,16 @@ function startGame(): void {
 function reset(): void {
     if (confirm("Are you sure you want to start a new game?")) {
         console.log("user consented to restart");
+
+        grid.clear();
+        ongoing = true;
+
+        //score stuff
+        if (score > parseInt(<string>localStorage.getItem("highscore"))) {
+            localStorage.setItem("highscore", score.toString());
+        }
+        score = 0;
+        displayScore(score, parseInt(<string>localStorage.getItem("highscore")));
     }
 }
 
@@ -44,12 +61,14 @@ function reset(): void {
  * @param {KeyboardEvent} e Details on the keypress
  */
 function keyHandler(e: KeyboardEvent): void {
-    if (e.key == "ArrowDown") {
-        console.log("down key pressed");
-    } else if (e.key == "ArrowLeft") {
-        console.log("left key pressed");
-    } else if (e.key == "ArrowRight") {
-        console.log("right key pressed");
+    if (ongoing) {
+        if (e.key == "ArrowDown") {
+            console.log("down key pressed");
+        } else if (e.key == "ArrowLeft") {
+            console.log("left key pressed");
+        } else if (e.key == "ArrowRight") {
+            console.log("right key pressed");
+        }
     }
 }
 
@@ -58,23 +77,53 @@ function keyHandler(e: KeyboardEvent): void {
  * @param {TouchEvent} e Details on the touch
  */
 function swipeHandler(e: TouchEvent): void {
-    if (e.type == "touchstart") {
-        touchX = e.touches[0].clientX;
-        touchY = e.touches[0].clientY;
-    } else if (e.type == "touchend") {
-        let xDiff = e.changedTouches[0].clientX - touchX;
-        let yDiff = e.changedTouches[0].clientY - touchY;
+    if (ongoing) {
+        if (e.type == "touchstart") {
+            touchX = e.touches[0].clientX;
+            touchY = e.touches[0].clientY;
+        } else if (e.type == "touchend") {
+            let xDiff = e.changedTouches[0].clientX - touchX;
+            let yDiff = e.changedTouches[0].clientY - touchY;
 
-        if (Math.abs(xDiff) > Math.abs(yDiff)) { //horizontal swipe
-            if (xDiff > 0) {
-                console.log("swiped right");
+            if (Math.abs(xDiff) > Math.abs(yDiff)) { //horizontal swipe
+                if (xDiff > 0) {
+                    console.log("swiped right");
+                } else {
+                    console.log("swiped left");
+                }
             } else {
-                console.log("swiped left");
-            }
-        } else {
-            if (yDiff > 0) {
-                console.log("swiped down");
+                if (yDiff > 0) {
+                    console.log("swiped down");
+                }
             }
         }
     }
+}
+
+/**
+ * Changes the current merge condition
+ */
+function changeCondition(): void {
+
+
+
+    printOnMessageBoard(currentCondition.toString());
+}
+
+/**
+ * Prints a message on the message board
+ * @param {string} message The message to print
+ */
+function printOnMessageBoard(message: string): void {
+    (<HTMLDivElement>document.getElementById("condition")).innerText = message;
+}
+
+/**
+ * Displays scores on the scoreboard
+ * @param {number} current The current score
+ * @param {number} highscore The high score
+ */
+function displayScore(current: number, highscore: number): void {
+    (<HTMLSpanElement>document.getElementById("score")).innerText = current.toString();
+    (<HTMLSpanElement>document.getElementById("highscore")).innerText = highscore.toString();
 }
