febdaed93f0dd41bd999ec35412fea38de6fdbe7
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-01-11 22:37:05 -0500

    some globals, plus reset stub

--- game.js
+++ game.js
@@ -1,14 +1,27 @@
 "use strict";
 let currentCondition;
 let grid;
+let score;
+let ongoing;
 /**
  * Sets up the page
  */
 function initializeGame() {
+    //set up the grid
     grid = new Grid(8, 8, (document.getElementById("grid")));
     startGame();
+    //scoreboard stuff
+    if (!localStorage.getItem("highscore")) {
+        localStorage.setItem("highscore", score.toString());
+    }
 }
 function startGame() {
+    score = 0;
+    ongoing = true;
     grid.dropRandomNumber();
+}
+function reset() {
+    if (confirm("Are you sure you want to start a new game?")) {
+    }
 }
 //# sourceMappingURL=game.js.map
--- game.ts
+++ game.ts
@@ -1,14 +1,30 @@
 let currentCondition: MergeCondition;
 let grid: Grid;
+let score: number;
+let ongoing: boolean;
 
 /**
  * Sets up the page
  */
 function initializeGame(): void {
-   grid = new Grid(8, 8, <HTMLDivElement>(document.getElementById("grid")));
-   startGame();
+    //set up the grid
+    grid = new Grid(8, 8, <HTMLDivElement>(document.getElementById("grid")));
+    startGame();
+
+    //scoreboard stuff
+    if (!localStorage.getItem("highscore")) {
+        localStorage.setItem("highscore", score.toString());
+    }
 }
 
 function startGame(): void {
+    score = 0;
+    ongoing = true;
     grid.dropRandomNumber();
+}
+
+function reset(): void {
+    if (confirm("Are you sure you want to start a new game?")) {
+
+    }
 }
