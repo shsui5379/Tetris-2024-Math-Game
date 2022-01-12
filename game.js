ca8ffde1c1c0b6f4d4ac6bbc4209ba54e3855c3b
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-01-11 23:19:43 -0500

    key and swipes

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ let currentCondition;
 let grid;
 let score;
 let ongoing;
+let touchX;
+let touchY;
 /**
  * Sets up the page
  */
@@ -10,6 +12,9 @@ function initializeGame() {
     //set up the grid
     grid = new Grid(8, 8, (document.getElementById("grid")));
     startGame();
+    //touch event
+    window.addEventListener("touchstart", swipeHandler);
+    window.addEventListener("touchend", swipeHandler);
     //scoreboard stuff
     if (!localStorage.getItem("highscore")) {
         localStorage.setItem("highscore", score.toString());
@@ -20,8 +25,54 @@ function startGame() {
     ongoing = true;
     grid.dropRandomNumber();
 }
+/**
+ * Ends the current game, and starts a new one
+ */
 function reset() {
     if (confirm("Are you sure you want to start a new game?")) {
+        console.log("user consented to restart");
+    }
+}
+/**
+ * Handles keypresses
+ * @param {KeyboardEvent} e Details on the keypress
+ */
+function keyHandler(e) {
+    if (e.key == "ArrowDown") {
+        console.log("down key pressed");
+    }
+    else if (e.key == "ArrowLeft") {
+        console.log("left key pressed");
+    }
+    else if (e.key == "ArrowRight") {
+        console.log("right key pressed");
+    }
+}
+/**
+ * Handles swipes
+ * @param {TouchEvent} e Details on the touch
+ */
+function swipeHandler(e) {
+    if (e.type == "touchstart") {
+        touchX = e.touches[0].clientX;
+        touchY = e.touches[0].clientY;
+    }
+    else if (e.type == "touchend") {
+        let xDiff = e.changedTouches[0].clientX - touchX;
+        let yDiff = e.changedTouches[0].clientY - touchY;
+        if (Math.abs(xDiff) > Math.abs(yDiff)) { //horizontal swipe
+            if (xDiff > 0) {
+                console.log("swiped right");
+            }
+            else {
+                console.log("swiped left");
+            }
+        }
+        else {
+            if (yDiff > 0) {
+                console.log("swiped down");
+            }
+        }
     }
 }
 //# sourceMappingURL=game.js.map
--- game.ts
+++ game.ts
@@ -3,6 +3,9 @@ let grid: Grid;
 let score: number;
 let ongoing: boolean;
 
+let touchX: number;
+let touchY: number;
+
 /**
  * Sets up the page
  */
@@ -10,6 +13,10 @@ function initializeGame(): void {
     //set up the grid
     grid = new Grid(8, 8, <HTMLDivElement>(document.getElementById("grid")));
     startGame();
+
+    //touch event
+    window.addEventListener("touchstart", swipeHandler);
+    window.addEventListener("touchend", swipeHandler);
 
     //scoreboard stuff
     if (!localStorage.getItem("highscore")) {
@@ -23,8 +30,51 @@ function startGame(): void {
     grid.dropRandomNumber();
 }
 
+/**
+ * Ends the current game, and starts a new one
+ */
 function reset(): void {
     if (confirm("Are you sure you want to start a new game?")) {
+        console.log("user consented to restart");
+    }
+}
 
+/**
+ * Handles keypresses
+ * @param {KeyboardEvent} e Details on the keypress
+ */
+function keyHandler(e: KeyboardEvent): void {
+    if (e.key == "ArrowDown") {
+        console.log("down key pressed");
+    } else if (e.key == "ArrowLeft") {
+        console.log("left key pressed");
+    } else if (e.key == "ArrowRight") {
+        console.log("right key pressed");
+    }
+}
+
+/**
+ * Handles swipes
+ * @param {TouchEvent} e Details on the touch
+ */
+function swipeHandler(e: TouchEvent): void {
+    if (e.type == "touchstart") {
+        touchX = e.touches[0].clientX;
+        touchY = e.touches[0].clientY;
+    } else if (e.type == "touchend") {
+        let xDiff = e.changedTouches[0].clientX - touchX;
+        let yDiff = e.changedTouches[0].clientY - touchY;
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) { //horizontal swipe
+            if (xDiff > 0) {
+                console.log("swiped right");
+            } else {
+                console.log("swiped left");
+            }
+        } else {
+            if (yDiff > 0) {
+                console.log("swiped down");
+            }
+        }
     }
 }
