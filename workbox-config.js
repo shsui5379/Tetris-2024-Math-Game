499e4a2238283a3d0914dfda18740eb77634dc5e
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-01-15 17:44:53 -0500

    don't cache jsons

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	globDirectory: './',
 	globPatterns: [
-		'**/*.{js,html,webmanifest,json,css}'
+		'**/*.{js,html,webmanifest,css}'
 	],
 	swDest: 'sw.js',
 	ignoreURLParametersMatching: [
