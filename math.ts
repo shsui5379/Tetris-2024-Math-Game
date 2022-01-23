beadc037be71783e55f1179c8bec76413a0f0cd4
shsui5379 <56182356+shsui5379@users.noreply.github.com>
2022-01-23 01:02:59 -0500

    fix some prime numbers being called composite

--- math.ts
+++ math.ts
@@ -7,7 +7,7 @@
 function isPrime(number: number): boolean {
    if (number <= 1) return false;
 
-   for (let i = 2; i < Math.sqrt(number); i++) {
+   for (let i = 2; i <= Math.sqrt(number); i++) {
       if (number % i === 0) return false;
    }
 
